ace8a0fffedf29e4bb5ea17ad2ed291c25488046
taranmistry <tjman1@live.com>
2016-05-06 10:14:19 -0700

    Added files via upload

---
+++ MainClassforTicTacToe.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mainclassfortictactoe;
+import java.io.*;
+import java.util.Scanner;        
+/**
+ *
+ * @author misttar17
+ */
+public class MainClassforTicTacToe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        char player='x'; 
+        int row ,column; 
+        Scanner k = new Scanner (System.in); 
+        Game g = new Game ();
+        
+        g.initizateGame(); 
+                
+        
+    }
+    
+}
