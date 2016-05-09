7e5bad9004bbedea1e0d17e43e58503d0e6a8fa9
taranmistry <tjman1@live.com>
2016-05-09 10:11:39 -0700

    Update MainClassforTicTacToe.java

--- MainClassforTicTacToe.java
+++ MainClassforTicTacToe.java
@@ -15,22 +15,48 @@ public class MainClassforTicTacToe {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+public static void main(String[] args) {
         char player='x'; 
         int row ,column; 
         Scanner k = new Scanner (System.in); 
         Game g = new Game ();
         
         g.initizateGame(); 
-        System.out.println ("Welcome to Tic-Tac-Toe!\n")      
+        System.out.println ("Welcome to a Tic-Tac-Toe Game!\n")      
         
-    while(truth)
+while(truth)
     }
     Player= g.ChangePlayer (Player); 
     System.out.print("\n + player +". choose your location (row, column:");
     row=k.nextInt();  
+    column=k.nextInt();
+    {
     
-        
+while("\n + player 2". chosse your location (row. column:");
+    g.displayboard();
+    row=k.nextInt();
+    column=k.nextInt();
+    
+while (g.checkIfLegal(row,column))
+    {
+    System.out.println("This place already has a X or O in it, choose a different spot, (row, column).");
+    g.displayBoard();
+    row=k.nextInt();
+    column=k.nextInt();
     }
-    
+g.changeBoard(player,row,column );
+g.displayBoard();
+
+if(g.checkIfWinner())
+    {
+    System.out.println("\nThe winner is "+ player+" !");
+    break;
+
+    }
+if(g.checkIfTie())
+    {
+        System.out.println("\nThe game is a tie !");
+        break;
+    }
 }
+
